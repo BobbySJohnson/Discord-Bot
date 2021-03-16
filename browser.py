52371bd12155107a7f506cd98dc6a953f14e557e
BobbySJohnson <BOBBYS.JOHNSON@student.csn.edu>
2021-03-16 09:19:17 +0000

    Add 0.5s

--- browser.py
+++ browser.py
@@ -16,8 +16,8 @@ async def take_screenshot(url, file_name, width, height):
       "width": width,
       "height": height
     })
-    await page.goto(url, {'waitUntil' : 'networkidle0'})
-    #await asyncio.sleep(1)
+    await page.goto(url)
+    await asyncio.sleep(0.5)
     await page.screenshot({'path': file_name})
     await page.close()
 
